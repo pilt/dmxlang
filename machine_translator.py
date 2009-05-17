4aff48125b133737bf40cc038c62dbf823310f58
Simon Pantzare <simon@spantz.org>
2009-05-17 11:59:23 +0200

    Rename code insertion method to 'insert' and make it possible to add several lines at a time.

--- machine_translator.py
+++ machine_translator.py
@@ -27,12 +27,15 @@ class Translate(translator.Translate):
         self.lines = []
         self.lineno = 0
 
-    def _a(self, line):
-        """Add a machine code line and increment the internal line number
+    def insert(self, line_or_lines):
+        """Add machine code lines and increment the internal line number
         counter. You should use this method and not operate on 'self.lines'
         directly. """
-        self.lineno += 1
-        self.lines.append(line)
+        if type(line_or_lines) == str:
+            line_or_lines = [line_or_lines]
+        for line in line_or_lines:
+            self.lineno += 1
+            self.lines.append(line)
 
     def on_do(self, do):
         """Write the machine code for a 'do' block. See 'DoStatement' in the
@@ -40,71 +43,71 @@ class Translate(translator.Translate):
         start_line = self.lineno
         if do.forever:
             start_label = label(start_line, 'do_forever')
-            self._a("%s : nop" % start_label)
+            self.insert("%s : nop" % start_label)
         else:
             start_label = label(start_line, 'do_%i_times' % do.times)
-            self._a("lda %s" % absarg(do.times))
-            self._a("%s : nop" % start_label)
-            self._a("get d0")
-            self._a("store d0 %s" % mem_counter(start_line))
+            self.insert("lda %s" % absarg(do.times))
+            self.insert("%s : nop" % start_label)
+            self.insert("get d0")
+            self.insert("store d0 %s" % mem_counter(start_line))
 
         for child in do.statements:
             self.walk(child)
 
         if do.forever:
-            self._a("jmp %s" % start_label)
+            self.insert("jmp %s" % start_label)
         else:
-            self._a("load d0 %s" % mem_counter(start_line))
-            self._a("put d0")
-            self._a("sub %s" % absarg(1))
-            self._a("get d0")
-            self._a("store d0 %s" % mem_counter(start_line))
+            self.insert("load d0 %s" % mem_counter(start_line))
+            self.insert("put d0")
+            self.insert("sub %s" % absarg(1))
+            self.insert("get d0")
+            self.insert("store d0 %s" % mem_counter(start_line))
             end_label = label(self.lineno + 2, 'end_do')
-            self._a("jmpz %s" % end_label)
-            self._a("jmp %s" % start_label)
-            self._a("%s : nop" % end_label)
+            self.insert("jmpz %s" % end_label)
+            self.insert("jmp %s" % start_label)
+            self.insert("%s : nop" % end_label)
     
     def on_to(self, to):
         """Write the machine code for a 'to' statement. See 'ToStatement' in the
         statements module to see 'to's properties."""
         # TODO: Implement other things. See IMPLEMENTATION.
         for (c, off) in zip([0, to.color.r, to.color.g, to.color.b, 0], range(5)):
-            self._a("lda %s" % absarg(c))
-            self._a("get d0")
-            self._a("store d0 %s" % channel(to.channel + off))
+            self.insert("lda %s" % absarg(c))
+            self.insert("get d0")
+            self.insert("store d0 %s" % channel(to.channel + off))
 
     def on_wait(self, wait):
         """Write the machine code for a 'wait' statement. See 'WaitStatement' in
         the statements module to see 'wait's properties."""
         # FIXME: Timing! 
         start = label(self.lineno, 'wait_%i' % wait.time)
-        self._a("lda %s" % absarg(wait.time))
-        self._a("get d0")
+        self.insert("lda %s" % absarg(wait.time))
+        self.insert("get d0")
         counter = mem_counter(self.lineno)
-        self._a("store d0 %s" % counter)
-        self._a("%s : nop" % start)
+        self.insert("store d0 %s" % counter)
+        self.insert("%s : nop" % start)
 
         # Inner loop.
         inner = label(self.lineno, 'inner_wait')
-        self._a("lda %s" % absarg(255))
-        self._a("%s : nop" % inner)
-        [self._a("nop") for _ in range(7)]
-        self._a("sub %s" % absarg(1))
+        self.insert("lda %s" % absarg(255))
+        self.insert("%s : nop" % inner)
+        [self.insert("nop") for _ in range(7)]
+        self.insert("sub %s" % absarg(1))
         inner_end = label(self.lineno + 2, 'inner_end')
-        self._a("jmpz %s" % inner_end)
-        self._a("jmp %s" % inner)
-        self._a("%s : nop" % label(self.lineno, 'inner_end'))
+        self.insert("jmpz %s" % inner_end)
+        self.insert("jmp %s" % inner)
+        self.insert("%s : nop" % label(self.lineno, 'inner_end'))
 
         # Outer check.
-        self._a("load d0 %s" % counter)
-        self._a("put d0")
-        self._a("sub %s" % absarg(1))
-        self._a("get d0")
-        self._a("store d0 %s" % counter)
+        self.insert("load d0 %s" % counter)
+        self.insert("put d0")
+        self.insert("sub %s" % absarg(1))
+        self.insert("get d0")
+        self.insert("store d0 %s" % counter)
         outer_end = label(self.lineno + 2, 'wait_end')
-        self._a("jmpz %s" % outer_end)
-        self._a("jmp %s" % start)
-        self._a("%s : nop" % outer_end)
+        self.insert("jmpz %s" % outer_end)
+        self.insert("jmp %s" % start)
+        self.insert("%s : nop" % outer_end)
 
     def end(self):
         """Called when we are finished parsing. Write output to standard
