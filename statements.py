27db67e7221b1ad5ae795844837d81f430004a00
Simon Pantzare <simon@spantz.org>
2009-05-18 16:08:54 +0200

    remove 'steps' property of 'to' statements

--- statements.py
+++ statements.py
@@ -24,21 +24,17 @@ class DoStatement(Statement):
 
 
 class ToStatement(Statement):
-    def __init__(self, color, time=None, 
-            steps=False, channel=1, from_color=None):
-
+    def __init__(self, color, time=None, channel=1, from_color=None):
         if time is None:
             time = 0
-
         self.color = color
         self.time = time
-        self.steps = steps
         self.channel = channel
         self.from_color = from_color
 
     def __repr__(self):
-        return "ToStatement(%r, time=%i, steps=%s, channel=%s, from_color=%r, indent=%i)" \
-            % (self.color, self.time, self.steps, self.channel, self.from_color, self.indent)
+        return "ToStatement(%r, time=%i, channel=%s, from_color=%r, indent=%i)" \
+            % (self.color, self.time, self.channel, self.from_color, self.indent)
 
 
 class WaitStatement(Statement):
