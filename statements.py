58852336372201799bb3e741270a7dc5f757bb24
Simon Pantzare <simon@spantz.org>
2009-05-18 16:22:02 +0200

    move 'RGB' from compile module to statements module, add 'RGBDiff'

--- statements.py
+++ statements.py
@@ -46,3 +46,27 @@ class WaitStatement(Statement):
     def __repr__(self):
         return "WaitStatement(time=%i, indent=%i)" \
             % (self.time, self.indent)
+
+
+class RGB(object):
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    def __repr__(self):
+        return "RGB(%i, %i, %i)" % (self.r, self.g, self.b)
+
+class RGBDiff(object):
+    def __init__(self):
+        self.r = self.g = self.b = 0
+
+    def diff(self, col1, col2):
+        for c in ['r', 'g', 'b']:
+            c1 = getattr(col1, c)
+            c2 = getattr(col2, c)
+            setattr(self, c, c1 - c2)
+        return self
+
+    def __repr__(self):
+        return "RGBDiff(%i, %i, %i)" % (self.r, self.g, self.b)
